8a2df2cf4d8d32a3fa3d2e958fbaa551e5c1e22b
Harsha-Gangadharan <harshaachu9685@gmail.com>
2024-02-27 13:18:04 +0530

    updated

--- bin/leap.dart
+++ bin/leap.dart
@@ -4,6 +4,6 @@ void leapyear(){
   print("enter a number");
   int year=int.parse(stdin.readLineSync()!);
   if(year%400==0){
-    print("leap year");
+    print(" $year is a leap year");
   }else if()
 }
